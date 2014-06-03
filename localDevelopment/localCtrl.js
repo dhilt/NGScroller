16563bd54786b0b5e43c10ce7ee76b68eb0a6b10
d.alexanov <d.alexanov@gmail.com>
2014-06-03 12:35:58 +0400

    cache local development

--- localDevelopment/localCtrl.js
+++ localDevelopment/localCtrl.js
@@ -1,83 +1,83 @@
 angular.module('application', ['ui.scroll', 'ui.scroll.jqlite']).controller('localCtrl', [
-  '$scope', '$log', '$timeout', function($scope, console, $timeout) {
-    var datasource;
+	'$scope', '$log', '$timeout', function ($scope, console, $timeout) {
+		var datasource;
 
-    datasource = {
+		datasource = {
 
-      cache: {
-        isEnabled: true,
+			cache: {
+				isEnabled: true,
 
-        items: {},
+				items: {},
 
-        toggle: function() {
-          this.isEnabled = !this.isEnabled;
-          this.items = {};
-        },
+				toggle: function () {
+					this.isEnabled = !this.isEnabled;
+					this.items = {};
+				},
 
-        saveItem: function(item, index) {
-          if(!this.isEnabled) {
-            return;
-          }
-          this.items[index] = item;
-        },
+				saveItem: function (item, index) {
+					if (!this.isEnabled) {
+						return;
+					}
+					this.items[index] = item;
+				},
 
-        get: function(index, count, success) {
-          if(!this.isEnabled) {
-            return;
-          }
+				get: function (index, count, success) {
+					if (!this.isEnabled) {
+						return;
+					}
 
-          var result = [];
-          var isCached = true;
+					var result = [];
+					var isCached = true;
 
-          for (var i = index; i < index + count; i++) {
-            if (!this.items.hasOwnProperty(i)) {
-              isCached = false;
-              return;
-            }
-            result.push(this.items[i]);
-          }
+					for (var i = index; i < index + count; i++) {
+						if (!this.items.hasOwnProperty(i)) {
+							isCached = false;
+							return;
+						}
+						result.push(this.items[i]);
+					}
 
-          success(result);
-          return true;
-        }
-      },
+					success(result);
+					return true;
+				}
+			},
 
-      get: function(index, count, success) {
-        var self = this;
+			get: function (index, count, success) {
+				var self = this;
 
-        if(self.cache.get(index, count, success)) {
-          return;
-        }
+				if (self.cache.get(index, count, success)) {
+					return;
+				}
 
-        $timeout(
-          function() {
-            var i, result, item;
+				$timeout(
+					function () {
+						var i, result, item;
 
-            result = [];
+						result = [];
 
-            for (i = index; i < index + count; i++) {
+						for (i = index; i < index + count; i++) {
 
-              if (i < -50 || i > 50) {
-                continue;
-              }
+							if (i < -50 || i > 50) {
+								continue;
+							}
 
-              item = {};
-              item.content = "item #" + i;
+							item = {};
+							item.content = "item #" + i;
 
-              result.push(item);
+							result.push(item);
 
-              self.cache.saveItem(item, i);
-            }
+							self.cache.saveItem(item, i);
+						}
 
-            success(result);
+						success(result);
 
-          }, 100);
-      }
+					}, 100);
+			}
 
-    };
+		};
 
 
-    $scope.datasource = datasource;
-  }
+		$scope.datasource = datasource;
+	}
 ]);
 
---
+++ src/examples/cache.coffee
@@ -0,0 +1,62 @@
+angular.module('application', ['ui.scroll', 'ui.scroll.jqlite']).controller('mainController',
+	[ '$scope', '$log', '$timeout'
+
+		($scope, console, $timeout)->
+
+			datasource = {}
+
+			datasource.cache = {
+				isEnabled: true
+
+				items: {}
+
+				toggle: () ->
+					this.isEnabled = not this.isEnabled
+					this.items = {}
+
+				saveItem: (item, index) ->
+					return if not this.isEnabled
+					this.items[index] = item
+
+				get: (index, count, success) ->
+					return if not this.isEnabled
+					result = []
+					isCached = true
+
+					for i in [index..index + count - 1] by 1
+						if not this.items.hasOwnProperty(i)
+							isCached = false
+							return
+					result.push(this.items[i])
+
+					success(result)
+					return true
+			}
+
+			datasource.get = (index, count, success)->
+				self = this
+
+				return if self.cache and self.cache.get(index, count, success)
+
+				$timeout(->
+					result = []
+
+					for i in [index..index + count - 1]
+						item = {}
+						item.content = "item ##{i}"
+						result.push item
+						self.cache.saveItem(item, i) if self.cache
+
+					success(result)
+					100
+				)
+
+			$scope.datasource = datasource
+
+	])
+
+angular.bootstrap(document, ["application"])
+
+###
+//# sourceURL=src/scripts/cache.js
+###

ae20e76788e83530927cbca721c3373d28cd6cd3
d.alexanov <d.alexanov@gmail.com>
2014-06-04 13:07:48 +0400

    local development clean

--- localDevelopment/localCtrl.js
+++ localDevelopment/localCtrl.js
@@ -1,81 +1,19 @@
-angular.module('application', ['ui.scroll', 'ui.scroll.jqlite']).controller('localCtrl', [
+angular.module('application', ['ui.scroll', 'ui.scroll.jqlite']).controller('mainController', [
 	'$scope', '$log', '$timeout', function ($scope, console, $timeout) {
-		var datasource;
+		var datasource = {};
 
-		datasource = {
-
-			cache: {
-				isEnabled: true,
-
-				items: {},
-
-				toggle: function () {
-					this.isEnabled = !this.isEnabled;
-					this.items = {};
-				},
-
-				saveItem: function (item, index) {
-					if (!this.isEnabled) {
-						return;
-					}
-					this.items[index] = item;
-				},
-
-				get: function (index, count, success) {
-					if (!this.isEnabled) {
-						return;
-					}
-
-					var result = [];
-					var isCached = true;
-
-					for (var i = index; i < index + count; i++) {
-						if (!this.items.hasOwnProperty(i)) {
-							isCached = false;
-							return;
-						}
-						result.push(this.items[i]);
-					}
-
-					success(result);
-					return true;
+		datasource.get = function (index, count, success) {
+			return $timeout(function () {
+				var i, item, result, _i, _ref;
+				result = [];
+				for (i = _i = index, _ref = index + count - 1; index <= _ref ? _i <= _ref : _i >= _ref; i = index <= _ref ? ++_i : --_i) {
+					item = {};
+					item.content = "item #" + i;
+					result.push(item);
 				}
-			},
-
-			get: function (index, count, success) {
-				var self = this;
-
-				if (self.cache.get(index, count, success)) {
-					return;
-				}
-
-				$timeout(
-					function () {
-						var i, result, item;
-
-						result = [];
-
-						for (i = index; i < index + count; i++) {
-
-							if (i < -50 || i > 50) {
-								continue;
-							}
-
-							item = {};
-							item.content = "item #" + i;
-
-							result.push(item);
-
-							self.cache.saveItem(item, i);
-						}
-
-						success(result);
-
-					}, 100);
-			}
-
+				return success(result);
+			}, 100);
 		};
-
 
 		$scope.datasource = datasource;
 	}
